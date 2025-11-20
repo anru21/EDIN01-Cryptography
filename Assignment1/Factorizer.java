7fd846fb81c0e379fc77db11c31206a09a569dd1
Andreas Ruggieri <anru.skola@gmail.com>
2025-11-20 14:58:02 +0100

    Commented code for GuassianElimination

--- Assignment1/Factorizer.java
+++ Assignment1/Factorizer.java
@@ -10,7 +10,7 @@ public class Factorizer {
 
         BigInteger N = new BigInteger("392742364277");
         System.out.println(N);
-        int[] factorBase = PrimeGenerator.genFactorBase(60000);
+        int[] factorBase = PrimeGenerator.genFactorBase(1000);
 
         System.out.print("Factorbase B: ");
         for (int i = 0; i < factorBase.length; i++) {
@@ -20,19 +20,7 @@ public class Factorizer {
 
         MatrixBundle bundle = Ygenerator.generate(factorBase, N);
 
-        System.out.println("hej");
-
         GaussianElimination.gaussAlgorithm(bundle, N, factorBase);
-
-        // Array Intialised and Assigned
-        // int[][] arr = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-
-        // Printing the Array
-        // for (int i = 0; i < 3; i++) {
-        // for (int j = 0; j < 2; j++)
-        // System.out.print(arr[i][j] + " ");
-        // System.out.println();
-        // }
 
     }
 }
--- Assignment1/GaussianElimination.java
+++ Assignment1/GaussianElimination.java
@@ -6,56 +6,31 @@ public class GaussianElimination {
 
         int[][] binaryMatrix = matrixBundle.exp_mod_mat();
         int[][] exponentMatrix = matrixBundle.exp_mat();
-        BigInteger[] xVector = matrixBundle.r_vector();
 
-        // int[][] binaryMatrix = { { 0, 1, 1 }, { 1, 0, 0 }, { 0, 1, 1 }, { 1, 0, 0 }
-        // };
-        // int[][] exponentMatrix = { { 2, 3, 1 }, { 3, 2, 0 }, { 0, 3, 5 }, { 1, 0, 2 }
-        // };
-        // BigInteger[] xVector = { BigInteger.ONE, BigInteger.TWO, BigInteger.TEN,
-        // BigInteger.ZERO };
+        BigInteger[] xVector = matrixBundle.r_vector();
 
         for (int i = 0; i < binaryMatrix[0].length; i++) { // Iterate through all factorbases (columns)
             for (int j = i; j < binaryMatrix.length; j++) { // Iterate through each equation (rows)
-                if (binaryMatrix[j][i] == 1) {
+                if (binaryMatrix[j][i] == 1) { // FIrst row with odd exponent for this factorbase
+
                     // Iterate from row j onward because all previous rows have even exponents for
                     // this factor base
                     for (int k = j; k < binaryMatrix.length; k++) {
                         if (k != j) {
                             if (binaryMatrix[k][i] == 1) { // Row have odd exponent for this factorbase
-                                // xVector[k] = xVector[k].multiply(xVector[j]);
-                                // for (int l = 0; l < binaryMatrix[0].length; l++) {
-                                // binaryMatrix[k][l] = (binaryMatrix[k][l] + binaryMatrix[j][l]) % 2;
-                                // exponentMatrix[k][l] = (exponentMatrix[k][l] + exponentMatrix[j][l]);
-                                // }
-                                accumulateRow(j, k, xVector, binaryMatrix, exponentMatrix, N); // Add row j to row k
+                                accumulateRow(j, k, xVector, binaryMatrix, exponentMatrix, N, factorBase); // Add row j
+                                                                                                           // to row k
                             }
                         }
                     }
 
-                    // Move up the vector with a 1 on the column we are visiting
-                    // int[] binaryTemp = binaryMatrix[j];
-                    // int[] exponentTemp = exponentMatrix[j];
-                    // BigInteger xTemp = xVector[j];
+                    moveUpPivotEquation(i, j, xVector, binaryMatrix, exponentMatrix); // Move up
 
-                    // binaryMatrix[j] = binaryMatrix[i];
-                    // exponentMatrix[j] = exponentMatrix[i];
-                    // xVector[j] = xVector[i];
+                    // System.out.println(binaryMatrix[k][i] + " " + exponentMatrix[k][i]);
+                    // pivot equation from
+                    // index j -> i
 
-                    // binaryMatrix[i] = binaryTemp;
-                    // exponentMatrix[i] = exponentTemp;
-                    // xVector[i] = xTemp;
-                    moveUpPivotEquation(i, j, xVector, binaryMatrix, exponentMatrix); // Move up pivot equation from
-                                                                                      // index j -> i
-                    // for (int s = 0; s < binaryMatrix.length; s++) {
-                    // for (int t = 0; t < binaryMatrix[s].length; t++) {
-                    // System.out.print(binaryMatrix[s][t]);
-                    // }
-                    // System.out.println();
-                    // }
-                    // System.out.println();
-
-                    break; // All rows should have 0 for this column index --> skip remaining
+                    // break; // All rows should have 0 for this column index --> skip remaining
                     // rows
                 }
             }
@@ -67,14 +42,15 @@ public class GaussianElimination {
             BigInteger y = BigInteger.valueOf(1);
             Boolean contains1 = false;
             for (int j = 0; j < exponentMatrix[i].length; j++) {
-                // System.out.print(binaryMatrix[i][j] + " ");
+                System.out.print(binaryMatrix[i][j] + " ");
                 if (binaryMatrix[i][j] == 1) {
                     contains1 = true;
                 }
-                y = y.multiply(BigInteger.valueOf(factorBase[j]).pow(exponentMatrix[i][j] / 2));
+                // System.out.println(exponentMatrix[i][j]);
+                y = y.multiply(BigInteger.valueOf(factorBase[j]).pow(exponentMatrix[i][j] /
+                        2));
             }
             // System.out.println(contains1);
-            // System.out.println();
             BigInteger modY = y.divideAndRemainder(N)[1];
 
             BigInteger gcd = bigIntGCD(modX.subtract(modY).abs(), N);
@@ -86,17 +62,27 @@ public class GaussianElimination {
         }
     }
 
+    /*
+     * Adds the equation for row j to equation for row k
+     */
     private static void accumulateRow(int j, int k, BigInteger[] xVector, int[][] binaryMatrix,
-            int[][] exponentMatrix, BigInteger N) {
+            int[][] exponentMatrix, BigInteger N, int[] factorBase) {
 
         xVector[k] = xVector[k].multiply(xVector[j]).mod(N);
-        System.out.println("Bit length of xVector[" + k + "]: " + xVector[k].bitLength());
+        // System.out.println("Bit length of xVector[" + k + "]: " +
+        // xVector[k].bitLength());
         for (int l = 0; l < binaryMatrix[0].length; l++) {
             binaryMatrix[k][l] = (binaryMatrix[k][l] + binaryMatrix[j][l]) % 2;
             exponentMatrix[k][l] = (exponentMatrix[k][l] + exponentMatrix[j][l]);
+            if (exponentMatrix[j][l] != 0) {
+                // System.out.println(factorBase[l] + " " + exponentMatrix[k][l]);
+            }
         }
     }
 
+    /*
+     * Swaps place between equation on row i and row j
+     */
     private static void moveUpPivotEquation(int i, int j, BigInteger[] xVector, int[][] binaryMatrix,
             int[][] exponentMatrix) {
 
