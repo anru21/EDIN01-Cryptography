6ad329d7105181b6a69446fbbd9bb5168c630ca9
Andreas Ruggieri <anru.skola@gmail.com>
2025-12-12 12:01:03 +0100

    Renamed some functions & variables

--- Assignment3/CorrelationAttack.java
+++ Assignment3/CorrelationAttack.java
@@ -1,5 +1,3 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -18,49 +16,59 @@ public class CorrelationAttack {
             }
         }
 
-        // D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 D10 D11 D12 D13
+        /***********************************************
+         * Finding initial state for LFSR13
+         *************************************************/
         int[] lf13Conn = { 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1 };
         LFSR lf13 = new LFSR(13, lf13Conn);
-        StringBuilder initState13 = initialStateGuess(lf13, 13, keystream);
+        StringBuilder initState13 = findMostLikelyInitialState(lf13, 13, keystream);
         System.out.println("The init state of LFSR13 is " + initState13);
+
+        /***********************************************
+         * Finding initial state for LFSR15
+         *************************************************/
         int[] lf15Conn = { 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1 };
         LFSR lf15 = new LFSR(15, lf15Conn);
-        StringBuilder initState15 = initialStateGuess(lf15, 15, keystream);
+        StringBuilder initState15 = findMostLikelyInitialState(lf15, 15, keystream);
         System.out.println("The init state of LFSR15 is " + initState15);
+
+        /***********************************************
+         * Finding initial state for LFSR17
+         *************************************************/
         int[] lf17Conn = { 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1 };
         LFSR lf17 = new LFSR(17, lf17Conn);
-        StringBuilder initState17 = initialStateGuess(lf17, 17, keystream);
+        StringBuilder initState17 = findMostLikelyInitialState(lf17, 17, keystream);
         System.out.println("The init state of LFSR17 is " + initState17);
 
     }
 
-    private static StringBuilder initialStateGuess(LFSR lfsr, int lfsrLength, StringBuilder keystream) {
+    private static StringBuilder findMostLikelyInitialState(LFSR lfsr, int lfsrLength, StringBuilder keystream) {
         float p_max = 0;
 
-        StringBuilder initStateGuess = new StringBuilder("");
+        StringBuilder bestState = new StringBuilder("");
 
-        for (int i = 1; i < Math.pow(2, lfsrLength) + 1; i++) {
-            int[] initState = new int[lfsrLength];
-            int lengthOfBinaryRep = Integer.toBinaryString(i).length();
-            String initialZeros;
+        for (int stateIndex = 1; stateIndex < Math.pow(2, lfsrLength) + 1; stateIndex++) {
+            int[] candiateState = new int[lfsrLength];
+            int binaryLength = Integer.toBinaryString(stateIndex).length();
+            String leadingZeros;
 
-            if (lengthOfBinaryRep < lfsrLength) {
+            if (binaryLength < lfsrLength) {
                 // Appends 0:s to the beginning of the binary representation if the number is
                 // less than lfsrLength bits
-                initialZeros = "0".repeat(lfsrLength - lengthOfBinaryRep);
+                leadingZeros = "0".repeat(lfsrLength - binaryLength);
             } else {
                 // The binary representation is lfsrLength bits long
-                initialZeros = "";
+                leadingZeros = "";
             }
 
-            StringBuilder binaryRepresent = new StringBuilder(initialZeros + Integer.toBinaryString(i));
+            StringBuilder binaryStateString = new StringBuilder(leadingZeros + Integer.toBinaryString(stateIndex));
 
             for (int j = 0; j < lfsrLength; j++) {
-                initState[j] = binaryRepresent.charAt((lfsrLength - 1) - j) - '0';
+                candiateState[j] = binaryStateString.charAt((lfsrLength - 1) - j) - '0';
             }
 
-            // Tests initStats as the initialstate of the lfsr
-            lfsr.setCurrPol(initState);
+            // Tests candiateState as the initialstate of the lfsr
+            lfsr.setCurrPol(candiateState);
 
             // Calculates the Hamming distance between the keystream and the LFSR output
             int hammingDistance = hammingDistance(lfsr, keystream);
@@ -70,14 +78,14 @@ public class CorrelationAttack {
 
             if (p > p_max) {
                 p_max = p;
-                initStateGuess = binaryRepresent;
+                bestState = binaryStateString;
             }
 
         }
 
         System.out.println("The best (maximum biased) probability p for LFSR" + lfsrLength + " is " + p_max);
 
-        return initStateGuess;
+        return bestState;
     }
 
     private static int hammingDistance(LFSR lfsr, StringBuilder keystream) {
@@ -89,7 +97,5 @@ public class CorrelationAttack {
         }
 
         return hammingDistance;
-
     }
-
 }
