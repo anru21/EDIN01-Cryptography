941572f7bc0a3f31685f8f469faf82b12150a05f
August Svensson <k.august.g.s@gmail.com>
2025-12-12 09:15:36 +0100

    Proj 3 step function

--- Assignment3/LFSR.java
+++ Assignment3/LFSR.java
@@ -20,11 +20,47 @@ public class LFSR {
         this.currPol = initState;
     }
 
+    public int[] getCurrPol() {
+        return this.currPol;
+    }
+
     public int getLength() {
         return length;
     }
 
     public int[] getConnectionPol() {
         return connectionPol;
+    }
+
+    public int step() {
+        int res = currPol[length - 1];
+        int temp = func();
+
+        for (int i = length - 1; i > 0; i--){
+            currPol[i] = currPol[i-1];
+        }
+
+        currPol[0] = temp;
+
+        return res;
+    }
+
+    private int func(){
+        int[] masked = new int[this.length];
+
+        for(int i = 0; i < length; i++){
+            masked[i] = currPol[i] & connectionPol[i]; 
+        }
+        
+        int c = 0;
+        for (int num : masked) {
+            if (num == 1) {
+                c++;
+            }
+        }
+
+        int res = c % 2;
+
+        return res;
     }
 }
